0efdd4d7422d8f3827e47d5a8c60dc3e7b74d1cd
Sherif-toiema22 <sherif.toiema22@gmail.com>
2025-10-13 23:36:31 +0300

    Switching into version 17

--- CourseDemo/src/app/app.ts
+++ CourseDemo/src/app/app.ts
@@ -10,3 +10,39 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('CourseDemo');
 }
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.css']
+})
+export class AppHeaderComponent {}
+
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  templateUrl: './app-sidebar.component.html',
+  styleUrls: ['./app-sidebar.component.css']
+})
+export class AppSidebarComponent {}
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {}
+
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: './app-footer.component.html',
+  styleUrls: ['./app-footer.component.css']
+})
+export class AppFooterComponent {}
